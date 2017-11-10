308b9f56829c896df0d27bc72a17e2ad7ea6de44
강상훈 <ishkang@gmail.com>
2017-11-10 09:33:30 +0900

    초기 커밋.

---
+++ SQLite.Interop/src/generic/interop.h
@@ -0,0 +1,18 @@
+/*
+ * interop.h -
+ *
+ * Written by Joe Mistachkin.
+ * Released to the public domain, use at your own risk!
+ */
+
+#ifndef INTEROP_VERSION
+#define INTEROP_VERSION          "1.0.106.0"
+#endif
+
+#ifndef INTEROP_SOURCE_ID
+#define INTEROP_SOURCE_ID        "daa1bd93214c2eb573ca9ecd1838c5e5f8eadd83"
+#endif
+
+#ifndef INTEROP_SOURCE_TIMESTAMP
+#define INTEROP_SOURCE_TIMESTAMP "2017-11-01 17:13:55 UTC"
+#endif
---
+++ System.Data.SQLite/SQLitePatchLevel.cs
@@ -0,0 +1,16 @@
+/********************************************************
+ * ADO.NET 2.0 Data Provider for SQLite Version 3.X
+ * Written by Joe Mistachkin (joe@mistachkin.com)
+ *
+ * Released to the public domain, use at your own risk!
+ ********************************************************/
+
+using System.Data.SQLite;
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceId("daa1bd93214c2eb573ca9ecd1838c5e5f8eadd83")]
+
+///////////////////////////////////////////////////////////////////////////////
+
+[assembly: AssemblySourceTimeStamp("2017-11-01 17:13:55 UTC")]
